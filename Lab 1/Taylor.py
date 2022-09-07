26f564e5155e5e26181135f287436c7e83181271
Marcos <41551000+Marcos7765@users.noreply.github.com>
2022-09-07 00:36:39 -0300

    Func. alt.; otim. opcional; leitor de csv
    me embolei pro final das funções alternativas, recomendo revisar;
    o que estava zoando o TaylorX era o termo da primeira derivada que tava avaliado em t;
    esse exemplo aqui usa uns números altões pra mostrar bem a otimização (eu fui testar com opt=false e já achei que tinha bugado a func dnv até lembrar dos 50*pi)
    lasanha = pegarDados('pos.csv')
    r = lasanha['r']
    altPlot(0, 300, 50*np.pi, True,exp=lasanha['pos'],termos=20,opt=True)

--- Lab 1/Taylor.py
+++ Lab 1/Taylor.py
@@ -1,13 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import factorial
+import csv
+
+def pegarDados(caminho):
+    '''Função para pegar os dados tabelados do experimento, caminho é o caminho
+    (path) do .csv'''
+    x, y = [], []
+    with open(caminho,'rt', newline="") as tabela:
+        leitor = csv.reader(tabela,)
+        for linha in leitor:
+            if linha[0] == 'Frame': continue
+            x.append(int(linha[1]))
+            y.append(int(linha[3]))
+    r = 40.90282037
+    return {'pos':np.array([x, y]), 'r':r}
 
 r = 1 #px
 
+#Xo e Yo servem caso se faça um imshow de um frame do vídeo no gráfico.
+#Xo e Yo exp. são pegarDados(caminho)['x'][0] e pegarDados(caminho)['y'][6].
 # Função X(t)
-def X(t: float) -> float: return r*(t-np.sin(t))
+def X(t: float, Xo:int =0) -> float: return r*(t-np.sin(t)) + Xo
 # Função Y(t)
-def Y(t: float) -> float: return r*(1-np.cos(t))
+def Y(t: float, Yo:int =0) -> float: return r*(1-np.cos(t)) + Yo
+
+def altTaylorX(t0: float, n: int, t: float, opt:bool = False) -> float:
+    if n <= 0: raise Exception("Número inválido de termos")
+    if opt: 
+        ciclo1 = altTaylorX(t0, n, (t - (np.floor((t)/(2*np.pi))*2*np.pi)))
+        return ciclo1 + r*(np.floor((t)/(2*np.pi))*2*np.pi)
+    a = 0  
+    for j in range(2,n):
+        i = n-j+1
+        if i%4==0:
+            a -= r*np.sin(t0)*(t-t0)**i/factorial(i)
+        if i%4==1:
+            a -= r*np.cos(t0)*(t-t0)**i/factorial(i)
+        if i%4==2:
+            a += r*np.sin(t0)*(t-t0)**i/factorial(i)
+        if i%4==3:
+            a += r*np.cos(t0)*(t-t0)**i/factorial(i)
+    if n > 1: a += r*(1-np.cos(t0))*(t-t0)
+    a += X(t0) #Termo inicial
+
+    return a
+
+def altTaylorY(t0: float, n: int, t: float, opt:bool = False) -> float:
+    if n <= 0: raise Exception("Número inválido de termos")
+    if opt: return altTaylorY(t0, n, (t - (np.floor((t)/(2*np.pi))*2*np.pi)) )
+    a = 0
+    for j in range(1,n):
+        i = n-j
+        if i%4==0:
+            a += -r*np.cos(t0)*((t-t0)**i)/factorial(i)
+        if i%4==1:
+            a += r*np.sin(t0)*((t-t0)**i)/factorial(i)
+        if i%4==2:
+            a += r*np.cos(t0)*((t-t0)**i)/factorial(i)
+        if i%4==3:
+            a += -r*np.sin(t0)*((t-t0)**i)/factorial(i)
+    a += Y(t0) #Termo inicial
+    return a
 
 def TaylorX(t0: float, n: int, t: float) -> float:
     a = X(t0) + r*(1-np.cos(t))*(t-t0) # Termo inicial    
@@ -43,3 +97,28 @@ def Plot(t0: float, n: int, t: float):
     plt.scatter(t_, y, label='y(t)')
     plt.legend()
     plt.show()
+    
+def altPlot(t0: float, n: int, t: float, ideal:bool =True, 
+            exp:np.ndarray =np.ndarray([]), **taylor):
+    '''os kwargs de taylor são o booleano opt e o inteiro termos que servem
+    para representar o n e o opt das func altTaylor eu troquei altTaylorX por
+    altTaylorY no opt do x e fiquei 30 min tentando entender como a distância
+    ficava zoada'''
+    t_ = np.linspace(t0, t, n+1) # Domínio
+    Xo, Yo = 0, 0
+
+    if exp.ndim==2:
+        plt.scatter(exp[0], exp[1], label='Dados experimentais')
+        Xo = exp[0][0]
+        Yo = exp[1][6]
+
+    if ideal: plt.plot(X(t_,Xo),Y(t_,Yo), label='Cicloide ideal')
+
+    if taylor:
+        plt.plot(altTaylorX(0, taylor['termos'], t_, taylor['opt']) + Xo,
+                 altTaylorY(0, taylor['termos'], t_, taylor['opt']) + Yo,
+                 label='Aprox. por Taylor')
+    plt.legend()
+    plt.axis('equal')
+    plt.show()
+    
